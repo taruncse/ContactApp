ad654846103538c9a0fc420fd3b214077ea7b846
Tarun <tarun.pstu@gmail.com>
2018-12-11 23:10:41 +0600

    * Added jackson Library * Added Json file * Parsing json file using util class

---
+++ app/src/main/java/contact/tkb/com/contactsapp/Utils.java
@@ -0,0 +1,39 @@
+package contact.tkb.com.contactsapp;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Utils {
+
+    public static List<ContactModel> loadJSONFromAsset(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("contact.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+            ObjectMapper mapper = new ObjectMapper();
+
+            List<ContactModel> myObjects = mapper.readValue(json, new TypeReference<List<ContactModel>>(){});
+            return myObjects;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
